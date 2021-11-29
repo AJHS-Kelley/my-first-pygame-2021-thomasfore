21bc4a6a15fd5facdb01b3747c424457b258b476
Thomas F <91069097+thomasfore@users.noreply.github.com>
2021-11-29 09:34:34 -0500

    [1a] v0.2
    added shapes and colors

--- .github/PyGamePractice.py
+++ .github/PyGamePractice.py
@@ -32,3 +32,13 @@ windowSurface.fill(WHITE)
 # Draw green polygon onto the surface.
 pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106),(236, 277),(56, 277), (0, 106)))
 
+# Draw blue lines on the windowSurface.
+pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+
+# Draw a circle.
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# Draw an ellipse.
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
